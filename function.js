69b6f15103fd7445c162dc5c03e64a61dab46fd7
Andrew Frazier <acorbettfrazier@gmail.com>
2020-02-13 16:10:31 -0500

    finished styling and guess counter

--- function.js
+++ function.js
@@ -2,6 +2,10 @@ let submit = document.querySelector("button");
 let letterBox = document.querySelector(".letter-container");
 let input = document.querySelector("input");
 let incorrectAnswer = document.querySelector(".wrong-letter-container");
+let wrongBox = document.querySelector("#h1-title");
+let guessTitle = document.querySelector("#guess-h1");
+let guessCounter = document.querySelector(".guess-counter");
+let counter = 10;
 
 // random word chosen
 let randomWords = [
@@ -63,18 +67,24 @@ function submitLetter(e) {
   console.log(singleLetter);
 
   blankLetters.forEach((letter, i) => {
-    console.dir(letter);
+    // console.dir(letter);
     if (singleLetter == letter.innerText) {
-      letter.style.fontSize = "20px";
+      letter.style.fontSize = "40px";
       letter.classList.add("correct");
+      letter.style.textTransform = "uppercase";
       input.value = "";
       correctLetter = true;
       wordBankValidator(blankLetters, document.querySelectorAll(".correct"));
       //
     } else if (i == blankLetters.length - 1 && correctLetter == false) {
       //this is so that the last inputted letter is then placed in the correct box
+      wrongBox.style.opacity = "1";
+      guessTitle.style.opacity = "1";
+      counter -= 1;
+      guessCounter.innerText = counter;
 
       wrongLetters.add(singleLetter);
+      loser();
       value = "";
 
       // this allows the values of set object to be shown
@@ -87,6 +97,12 @@ function submitLetter(e) {
   });
 }
 //
+const loser = () => {
+  if (counter === 0) {
+    alert("LOSER");
+  }
+};
+
 const wordBankValidator = (word, answered) => {
   // --> word: array of the word
   if (word.length === answered.length) {
